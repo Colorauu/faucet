e1f7cf3e04074e0b4b7c74bf848d6bb17745b0ea
Colorauu <colorauu.teste@gmail.com>
2025-11-26 01:06:13 -0300

    Delete test_db.php

--- test_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "app/core/Database.php";
-
-$db = Database::getConnection();
-$stmt = $db->query("SELECT id, username, email, password FROM admins LIMIT 1");
-print_r($stmt->fetch(PDO::FETCH_ASSOC));
