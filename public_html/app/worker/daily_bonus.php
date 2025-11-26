a3d71a712a4f4c336b2b1ee54498180692c6e3b4
Colorauu <colorauu.teste@gmail.com>
2025-11-26 01:17:41 -0300

    Delete public_html directory

--- public_html/app/worker/daily_bonus.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../core/Database.php';
-$db = Database::getConnection();
-$db->query("UPDATE users SET balance = balance + 0.01 WHERE DATE(created_at) = CURDATE()"); // example
-echo "daily ok";
--- public_html/test_password.php
+++
@@ -1,9 +0,0 @@
-<?php
-$senhaPura = "admin";
-$hash = '$2y$10$Uv27eLJXEx5sybh.wHpUpO2vswztOMh7mDBT6RyZE3mHfp5xP/VXm';
-
-if (password_verify($senhaPura, $hash)) {
-    echo "OK - SENHA CONFERE!";
-} else {
-    echo "ERRO - SENHA NÃO CONFERE!";
-}
